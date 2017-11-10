f6e8e1bc8a89b04a03745636bd6e354b7a3f81a5
Peter Chenery <peter.chenery@anthonynolan.org>
2017-11-10 22:31:07 +0000

    Initial dictionary

--- TLS/TLS/Program.cs
+++ TLS/TLS/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -11,7 +13,7 @@ namespace TLS
         static void Main(string[] args)
         {
             string path = @"c:\work\training\tls\sampletext.txt";
-
+ 
             //Open the file to read from.
             string readText = File.ReadAllText(path);
 
@@ -19,35 +21,20 @@ namespace TLS
             Regex rx = new Regex(@"tra",
                 RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-            // Find matches.
             MatchCollection matches = rx.Matches(readText);
 
             // Report the number of matches found.
-            Console.WriteLine("{0} matches found: ",
-                matches.Count);
+             Console.WriteLine("{0} matches found: ",
+                 matches.Count);
 
-            // Report on each match.
-            foreach (Match match in matches)
-            {
-                GroupCollection groups = match.Groups;
-                Console.WriteLine("'{0}' repeated at positions {1} and {2}",
-                    groups["word"].Value,
-                    groups[0].Index,
-                    groups[1].Index);
-            }
-
-            //int counter = 0;
-
-            //for (int i = 0; i < readText.Length; i++)
-            //{
-            //    if (readText[i] == 't' && readText[i + 1] == 'r' && readText[i + 2] == 'a')
-            //    {
-            //        counter++;
-            //    }
-            //}
-
-            //Console.WriteLine(counter);
+            var dict = Regex.Matches(readText, @"\w{3}")
+                .Cast<Match>().GroupBy(o => o.Value)
+                .ToDictionary(o => o.Key, o => o.Count());
+ 
+            Console.WriteLine(dict["tra"]);
+            Console.WriteLine(dict["pre"]);
             Console.ReadLine();
+
         }
     }
 }
